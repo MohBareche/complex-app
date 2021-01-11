f13d8508e938a0daca93690f6572025ecb2bcf96
moh bareche <mbareche.bic@gmail.com>
2021-01-10 22:59:32 -0500

    'second

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 const mongodb = require("mongodb");
-
 
 mongodb.connect(
   process.env.CONNECTIONSTRING,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err, client) => {
-    module.exports = client
-    const app = require('./app')
-    const port = process.env.PORT
+    module.exports = client;
+    const app = require("./app");
+    const port = process.env.PORT || 8080;
     app.listen(port, () => {
-        console.log(`Server started on port 3000 and connected to database...`);
+      console.log(`Server started on port 3000 and connected to database...`);
     });
   }
 );
